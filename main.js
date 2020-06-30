96eed21dfd92112fdde4b136fa0ebe82137057a2
Elliehernan1993 <elliehernan1993@gmail.com>
2020-06-30 08:19:23 -0400

    commit test

---
+++ main.js
@@ -0,0 +1,26 @@
+let coin = {
+    state: 0,
+    flip: function() {
+        /* 1. Randomly set your coin object's "state" property to be either 
+           0 or 1: use "this.state" to access the "state" property on this object.*/
+           this.state = Math.trunc(Math.random() * 2)
+    },
+    toString: function() {
+        /* 2. Return the string "Heads" or "Tails", depending on whether
+           "this.state" is 0 or 1. */
+        if (this.state === 1) return "Heads" 
+        return "Tails"
+    },
+    toHTML: function() {
+        let image = document.createElement('img');
+        /* 3. Set the properties of this image element to show either face-up
+           or face-down, depending on whether this.state is 0 or 1.*/
+           
+
+        
+        return image;
+    }
+};
+  
+
+        
